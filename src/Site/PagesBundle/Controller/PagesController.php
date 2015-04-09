f799291773fb54521d326a6f5f7a7a01191f3165
Gauthier <gauthier.desprets@alterway.fr>
2015-04-09 16:44:47 +0200

    Split the News page in 2 parts and modify the footer and the header for some page

--- src/Site/PagesBundle/Controller/PagesController.php
+++ src/Site/PagesBundle/Controller/PagesController.php
@@ -9,18 +9,26 @@ class PagesController extends Controller
     public function indexAction()
     {
         return $this->render('PagesBundle::index.html.twig');
-    }          
-        public function actualitesAction()
+    }
+
+    public function actualitesAction()
     {
         return $this->render('PagesBundle::actualites.html.twig');
-    } 
-        public function fichesVoyagesAction()
+    }
+
+    public function fichesVoyagesAction()
     {
         return $this->render('PagesBundle::fiche_voyage.html.twig');
     }
 
-        public function carnetDeBordAction()
-        {
-            return $this->render('PagesBundle::carnet_de_bord.html.twig');
-        }
+    public function carnetDeBordAction()
+
+    {
+        return $this->render('PagesBundle::carnet_de_bord.html.twig');
+    }
+
+    public function actualitesSuiteAction()
+    {
+        return $this->render('PagesBundle::actualites_suite.html.twig');
+    }
 }
